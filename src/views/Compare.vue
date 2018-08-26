40ad781685638c726a5c4099523f66bdc5081c5d
bieleckim <marekbielecki93@gmail.com>
2018-08-26 16:20:19 +0200

    Simple Comparison

--- src/views/Compare.vue
+++ src/views/Compare.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="compare">
-        Compare {{ first }} with {{ second }}.
+        <div class="container">
+            <div v-for="(comparison, index) in comparisons" :key="index">
+                <h5 class="text-gray mt-2">{{ comparison.name }}</h5>
+                <div class="columns" :class="{ 'text-success': comparison.isSame }">
+                    <div class="column col-6">
+                        {{ comparison.first }}
+                    </div>
+                    <div class="column col-6">
+                        {{ comparison.second }}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,6 +22,37 @@
         props: {
             first: String,
             second: String
+        },
+        data() {
+            return {
+                firstUrl: null,
+                secondUrl: null,
+                comparisons: []
+            };
+        },
+        created() {
+            this.firstUrl = new URL(this.first);
+            this.secondUrl = new URL(this.second);
+            this.createSimpleComparison('Protocol', 'protocol');
+            this.createSimpleComparison('Hostname', 'hostname');
+            this.createSimpleComparison('Port', 'port');
+        },
+        methods: {
+            createSimpleComparison(name, property) {
+                this.comparisons.push({
+                    name: name,
+                    first: this.firstUrl[property],
+                    second: this.secondUrl[property],
+                    isSame: this.firstUrl[property] === this.secondUrl[property],
+                });
+            },
         }
     }
 </script>
+
+<style scoped>
+    h5 {
+        text-transform: uppercase;
+        font-size: 14px;
+    }
+</style>
